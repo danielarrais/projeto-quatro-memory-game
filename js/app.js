f90ffbccf269f92aec496fc9d872c724b0f52120
Daniel Arrais <danielarraiscarvalho@gmail.com>
2019-04-14 11:56:37 -0300

    Embaralhamento das cartas
    Função criada para fazer a adição do cards embaralhados na tela

--- js/app.js
+++ js/app.js
@@ -25,6 +25,15 @@ function shuffle(array) {
     return array;
 }
 
+function prepararCards(cards){
+    let deck = $('.deck')
+
+    shuffle(cards);
+
+    deck.empty();
+
+    deck.append(cards)
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +45,8 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+const cards = $('.card');
+
+prepararCards(cards);
+
